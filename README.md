0a310df847ec2b5f7e325b9a391cde8e93011e00
n18011 <n18011@std.it-college.ac.jp>
2019-12-03 14:49:48 +0900

    edit Example Usage in README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # useAddToHomeScreen
+
+## Example Usage
+```javascript
+import React, {useState, useEffect} from 'react'
+import {useAddToHomeScreen} from './useAddToHomeScreen'
+
+
+export default () => {
+  const [prompt, promptToInstall] = useAddToHomeScreen()
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    if (prompt) {
+      setVisible(true)
+    }
+
+  }, [prompt])
+
+  return (
+    {!visible ? <div/>: <button onClick={promptToInstall}>Add to home screen</button>}
+  )
+}
+
+```
--- useAddToHomeScreen.js
+++ useAddToHomeScreen.js
@@ -1,1 +1,31 @@
+import React, { useState, useEffect } from 'react'
 
+export default () => {
+  const [prompt, setPrompt] = useState(null)
+
+  const promptToInstall = () => {
+    if (prompt) {
+      return prompt.prompt()
+    }
+    return Promise.reject(
+      new Error(
+        'ブラウザに"beforeinstallprompt"イベントを送る前にインストールしてください'
+      )
+    )
+  }
+
+  useEffect(() => {
+    const ready = (e) => {
+      e.preventDefault()
+      setPrompt(e)
+    }
+
+    window.addEventListener('beforeinstallprompt', ready)
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', ready)
+    }
+  }, [])
+
+  return [prompt, promptToInstall]
+}
